91a8e9049f24c0accf362f4dc172133ce54c4dee
Victor Hernandez <asistencia@bcnschool.cl>
2022-07-07 15:53:45 -0400

    usando plantillas como herencias para mostrar secciones y titulos en las vistas

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>contact</title>
-</head>
-<body>
-    @include('partials.navigation')
-    <h1>contact  </h1>
-    
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title','contacto')
+
+@section('content')
+<h1>contact  </h1>
+@endsection
+
