b24de93fa02cd767fa81412a5976dae5215c3072
Victor Hernandez <hernandez.victormanuel@gmail.com>
2022-07-29 06:54:43 -0400

    feact: using D.R.Y

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\SavePostRequest;
 
 
 class PostController extends Controller
@@ -16,43 +17,29 @@ class PostController extends Controller
 
     public function show(Post $post)
     {
-        return view('posts.show', ['post'=> $post]); 
+        return view('posts.show',['post'=> $post]); 
     }
     public function create()
     {
-        return view('posts.create'); 
+        return view('posts.create',['post'=> new Post]); 
     }
-    public function store(Request $request)
+    public function store(SavePostRequest $request)
     {
-        $request->validate([
-            'title' => ['required', 'min:4'],
-            'body'  => ['required'],
-        ]);
-        $post = new Post();
-        $post->title = $request->input('title');
-        $post->body = $request->input('body');
-        $post->save();
+         Post::create($request->validated());
         session()->flash('status','Post Creado!!!');
 
         return to_route('posts.index');
     }
     public function edit(Post $post)
     {
-        return view('posts.edit', ['post'=> $post]); 
+        return view('posts.edit',['post'=> $post]); 
     }
-    public function update(Request $request, Post $post)
+    public function update(SavePostRequest $request, Post $post)
     {
-        $request->validate([
-            'title' => ['required', 'min:4'],
-            'body'  => ['required'],
-        ]);
-    
-        $post->title = $request->input('title');
-        $post->body = $request->input('body');
-        $post->save();
+        $post->update($request->validated());
         session()->flash('status','Post Actualizado!!!');
 
-        return to_route('posts.show', $post);
+        return to_route('posts.show',$post);
     }
 }
 
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,22 +2,7 @@
     <h1>Crear nuevo Post</h1>
     <form method="POST" action="{{ route('posts.store')}}">
         @csrf
-        <label> 
-            Titulo<br>
-            <input type="text" name="title" value={{ old('title')}}>
-            @error('title')
-                <br>
-                <small style="color:red">{{ $message }}</small>
-            @enderror
-        </label><br>
-        <label> 
-            Body<br>
-            <textarea name="body">{{old('body')}}</textarea>
-            @error('body')
-            <br>
-            <small style="color:red">{{ $message }}</small>
-        @enderror
-        </label><br>
+        @include('posts.form-fields')
         <button type="submit">Enviar</button>
         <br>
     </form>
