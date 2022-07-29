1cb9360749092a8bff4505d37c74d1d305d752ce
Victor Hernandez <hernandez.victormanuel@gmail.com>
2022-07-29 05:52:24 -0400

    feact: add files es-lang

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,14 @@ return [
     |
     */
 
+    'password' => 'Password must have 6 caracters y coincidir.',
     'reset' => 'Your password has been reset!',
     'sent' => 'We have emailed your password reset link!',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
+
+    
+    
 
 ];
