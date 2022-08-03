25af9df29390590701e79de0825520337e9c6c83
Victor Hernandez <hernandez.victormanuel@gmail.com>
2022-08-03 16:11:55 -0400

    feact: add css to  post

--- resources/views/posts/form-fields.blade.php
+++ resources/views/posts/form-fields.blade.php
@@ -2,19 +2,17 @@
     <div class="mb-3">
         <label for="InputNombre" class="form-label">Titulo</label>
         <input type="text" class="form-control" id="InputNombre" aria-describedby="nombreHelp" name="title" value={{old('title',$post->title)}}>
-        <div id="nombreHelp" class="form-text">Debes colocar un Titulo</div>
         @error('title')
         <br>
         <small style="color:red">{{ $message }}</small>
-    @enderror
+        @enderror
+    </div>
+
+    <div class="mb-3">
+        <label for="InputDescripcion" class="form-label">Body</label>
+        <textarea name="body" class="form-control">{{old('body',$post->body)}}</textarea>
+        @error('body')
+        <small style="color:red">{{ $message }}</small>
+        @enderror
     </div>
 </div>
-
-<label> 
-    Body<br>
-    <textarea name="body">{{old('body',$post->body)}}</textarea>
-    @error('body')
-    <br>
-    <small style="color:red">{{ $message }}</small>
-@enderror
-</label><br>
